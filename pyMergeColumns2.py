cbc90af08e07a2a57130dade9bc499784b2490a5
Matteo <matpaolieri@gmail.com>
2021-03-20 20:37:50 +0100

    update

--- pyMergeColumns2.py
+++ pyMergeColumns2.py
@@ -1,21 +1,18 @@
 #!/usr/bin/env python3
 
-
 import pandas as pd
 import argparse
-
+import io
  
-def merge(output_file, input_files, symbol_delimiter):
+def merge(output_file, files, symbol_delimiter):
     
     dataset = pd.DataFrame()
 
-    files = input_files
-
-    print(f'Files to merge (in this order): {files}\n')
+    print(f'\nFiles to merge (in this order): {files}\n')
     
     for f in files:
-        
-        print(f'Reading {f}...')
+                
+        print(f'\nReading {f}...\n')
         
         df_new = pd.read_csv(f, delimiter=symbol_delimiter)
         dataset = pd.concat([dataset,df_new], axis = 1)
@@ -25,18 +22,15 @@ def merge(output_file, input_files, symbol_delimiter):
     
 def main(output_file, input_files, symbol_delimiter):
     
-    try:
         merge(output_file, input_files, symbol_delimiter)
-    except Exception as e:
-        print(f'ERROR: {e}')
 
 
 if __name__ == "__main__":
     
     parser = argparse.ArgumentParser(description='Merge data files.')
-    parser.add_argument('-output', nargs=1, type=str, help="Output file name (remember to write extension!)")
-    parser.add_argument('-input', nargs='+', type=str, help="Input file names")
-    parser.add_argument('-symbol', type=str, help="Symbol used as delimiter. Default is tab '\t'. Can be used: " ", . ' ' etc.")
+    parser.add_argument('-o', '--output', nargs=1, type=str, help="Output file name (remember to write extension!)")
+    parser.add_argument('-i', '--input', nargs='+', type=str, help="Input file names")
+    parser.add_argument('-s', '--symbol', type=str, help="Symbol used as delimiter. Default is tab '\t'. Can be used: " ", . ' ' etc.")
                                                    
     parser.set_defaults(empty=0)
     parser.set_defaults(symbol='\t')
@@ -44,4 +38,4 @@ if __name__ == "__main__":
     
     args = parser.parse_args()
     
-    main(args.output, args.input, args.symbol)
+    main(args.output[0], args.input, args.symbol)
